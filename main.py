550836cfa49517ec3ac167d41b5c8a7ea43af33b
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2021-11-11 22:12:15 +0100

    added cors

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import uvicorn
 from fastapi import FastAPI
 from controller import auth_controller, user_controller, question_controller
 from data.data_init import init_data
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(auth_controller.router)
 app.include_router(user_controller.router)
 app.include_router(question_controller.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_data()
 
